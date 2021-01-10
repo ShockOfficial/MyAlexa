47235f75b604c1dbf14027b25638a5c31b8f6792
Paul Hoe <pglmotyka@gmial.com>
2021-01-10 19:23:45 +0100

    Nowe funkcje Alexy

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import wikipedia
+import subprocess
+import playsound
 
 def take_comand():
     listener = sr.Recognizer()
@@ -19,12 +21,13 @@ def take_comand():
 
 
     except:
-        talk("Musisz mówić do mnie po imieniu!")
+        #talk("Musisz mówić do mnie po imieniu!")
+        #take_comand()
+        pass
 
 
 def skills(command):
     respsond = "Nie umiem tego jeszcze zrobić"
-
     if "umyj grzyby" in command:
         respsond = "Hahaha! Bardzo zabawne."
 
@@ -55,10 +58,47 @@ def skills(command):
         pywhatkit.playonyt(song)
         respsond = "Włączam " + song
 
-    elif "wyszukaj" in command or "szukaj" in command:
+    elif "wyszukaj" in command or "szukaj" in command or "google" in command:
         command = command.replace("wyszukaj","")
         respsond = "Dobrze, szukam w google."
         pywhatkit.search(command)
+
+    elif "muszę cię zabić" in  command:
+        respsond = "Jeżeli musisz...\nCzyń swoją powinność.\nPa."
+        print("[Alexa]: Jeżeli musisz...\n[Alexa]: Czy swoją powinność.\n[Alexa]: Pa.")
+        talk(respsond)
+        exit()
+
+    elif "wyłącz się" in command:
+        respsond = "Okej no to zmykam. Pa."
+        print("[Alexa]:",respsond)
+        talk(respsond)
+        exit()
+
+    elif "powiedz od tył" in command:
+        command = command.replace("powiedz od tył", "")
+        respsond = "No to słuchaj:" + command[::-1]
+
+    elif "już nic" in command:
+        respsond = "A co ja mam ze sobą zrobić?"
+        print("[Alexa]:",respsond)
+        talk(respsond)
+        skills(take_comand())
+
+
+    elif "kalkulator" in command:
+        respsond = "Proszę bardzo. Uruchamiam kalkulator."
+        subprocess.call("calc.exe")
+    elif "speak english" in command:
+        respsond = "Yes of course i can speak English."
+    # elif "włącz mój cover" in command:
+    #     respsond = "Włączam!"
+    #     playsound.playsound("C:\\Users\\pawci\\Desktop\\Piosenki\\Kortez - Pierwsza cover Motyka_P.wav",block=False)
+    # elif "wyłącz muzykę" in command:
+    #     playsound.playsound("C:\\Users\\pawci\\Desktop\\Piosenki\\Kortez - Pierwsza cover Motyka_P.wav")
+    #     respsond = "Wyłączam."
+
+
 
     return respsond
 
@@ -71,16 +111,28 @@ def talk(command):
     engine.runAndWait()
 
 def run_alexa():
-    print("Słucham...")
-    print("W czym mogę ci pomóc?")
-    talk("Słucham")
-    talk("W czym moge ci pomóc?")
+    if first_time:
+        print("[Alexa]:","Słucham...")
+        print("[Alexa]:","W czym mogę ci pomóc?")
+        talk("Słucham")
+        talk("W czym moge ci pomóc?")
+    else:
+        print("[Alexa]:","Co jeszcze mam dla ciebie zrobić?")
+        talk("Co jeszcze mam dla ciebie zrobić?")
 
     command = take_comand()
-    print(command)
+    print("[Ty]:",command)
     respond = skills(command)
-    print(respond)
+    print("[Alexa]:",respond)
     talk(respond)
 
 
-run_alexa()
+first_time = True
+
+while True:
+
+    if first_time:
+        run_alexa()
+        first_time = False
+    else:
+        run_alexa()
